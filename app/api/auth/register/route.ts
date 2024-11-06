fb7a9ef4b44bbcd1a1d91472ec4578eaf0609173
weikangg <chongweikang5@gmail.com>
2024-11-07 00:11:16 +0800

    test deployment

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -20,6 +20,7 @@ export async function POST(request: Request) {
     );
     const user = userCredential.user;
 
+    
     // Store additional user info in Firestore
     const userRef = await addDoc(collection(db, "users"), {
       uid: user.uid,
