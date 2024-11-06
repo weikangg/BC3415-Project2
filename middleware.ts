676b0f896cd502aa7bda081f29e7194d95598098
Kane <kanetan4@gmail.com>
2024-11-06 23:02:21 +0800

    Comment out login

--- middleware.ts
+++ middleware.ts
@@ -4,17 +4,17 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  if (
-    (pathname === "/login" || pathname === "/register") &&
-    request.cookies.has("userAuth")
-  )
-    return NextResponse.redirect(new URL("/", request.url));
+  // if (
+  //   (pathname === "/login" || pathname === "/register") &&
+  //   request.cookies.has("userAuth")
+  // )
+  //   return NextResponse.redirect(new URL("/", request.url));
 
-  if (
-    (pathname === "/" || pathname === "/accounts") &&
-    !request.cookies.has("userAuth")
-  )
-    return NextResponse.redirect(new URL("/login", request.url));
+  // if (
+  //   (pathname === "/" || pathname === "/accounts") &&
+  //   !request.cookies.has("userAuth")
+  // )
+  //   return NextResponse.redirect(new URL("/login", request.url));
 
   return NextResponse.next();
 }
